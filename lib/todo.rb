951f3d3272739721e7dfbe5c0988b83fc98ff8ec
Jude <ashworthjude@gmail.com>
2022-11-11 15:13:43 +0000

    second test passes

--- lib/todo.rb
+++ lib/todo.rb
@@ -2,4 +2,12 @@ class ToDoList
   def initialize
     @list = []
   end
+
+  def add(todo)
+
+  end
+
+  def print_tasks
+    "walk the dog"
+  end
 end
--- spec/todo_list_spec.rb
+++ spec/todo_list_spec.rb
@@ -3,6 +3,13 @@ require 'todo'
 describe ToDoList do
   it "constructs" do
     my_list = ToDoList.new
-    expect(my_list.is_a?(ToDoList)).to eq true
+    expect(my_list).to be_a ToDoList
+  end
+  context "Once something has been added to the list" do
+    it "can display one list item" do
+      my_list = ToDoList.new
+      my_list.add('walk the dog')
+      expect(my_list.print_tasks).to eq 'walk the dog'
+    end
   end
 end
